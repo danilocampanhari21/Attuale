8513bc7afc603b3ce4f240d26574ecc5e88ea299
Danilo Campanhari <101272141+Fimdomundo21@users.noreply.github.com>
2022-11-26 22:24:31 +0100

    Add files via upload

---
+++ src/components/ProjectCard1.js
@@ -0,0 +1,15 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard1 = ({ title, description, imgUrl}) => {
+    return (
+        <Col sm={6} md={4}>
+            <div className="proj-imgbox">
+                <img src={imgUrl} />
+                <div className="proj-txtx">
+                    <h4>{title}</h4>
+                    <span>{description}</span>
+                </div>
+            </div>
+        </Col>
+    )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,136 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+
+
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+
+    
+    
+  ];
+
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Progetti</h2>
+                <p>Alcune delle funzioni più importanti nel Marketing sono la creazione di strutture importanti, come possiamo vedere di seguito alcuni dei nostri lavori già svolti</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Sitio</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Aplicazione</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Altri servizi</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                    <p>La creazione di un sito Web è un must per i clienti nuovi ed esistenti per sapere cosa hai da offrire.</p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <p>La creazione di un'applicazione serve a dimostrare la serietà dell'azienda che il cliente sta acquistando, in quanto dimostra fiducia, praticità e sicurezza per il cliente.</p>
+                    <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+
+                  </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <p>
+                       API<br></br>
+                       Creazione di Software<br></br>
+                       Aggiornamenti Software<br></br>
+                       Sicurezza informatica<br></br>
+                    </p>
+
+                    <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
